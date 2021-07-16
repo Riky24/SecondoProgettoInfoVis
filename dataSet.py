06784e2d7dac442125fb244d188545ffd0d4f998
Riccardo <riky24.mo@gmail.com>
2021-07-16 13:00:50 +0200

    Prima visualizzazione dei dati

--- dataSet.py
+++ dataSet.py
@@ -8,16 +8,19 @@ P1 = 0.5  #probabilità di biforcazione
 P2 = 0.5  #probabilità di tagliare la biforcazione
 
 bif = 0  #numero di biforcazioni
+ID = 0
 
 #funzione che dato il blocco precedente e la relativa altezza genera un blocco figlio
 def creaBlocco(b_pred, h_pred):
+	global ID
 	if h_pred>=0:  #se non è il nodo radice
 		hash_pred = b_pred.get('hash')
 	else:  #se è il nodo radice
 		hash_pred = ""
 	height = h_pred+1
-	hash_val = hash("block"+str(height)+str(hash_pred))
-	block = {"hash":hash_val,"prev_block":hash_pred,"height":height, "height_pred":h_pred}
+	hash_val = hash("block"+str(height)+str(hash_pred)+str(ID))
+	block = {"hash":hash_val,"prev_block":hash_pred,"height":height, "height_pred":h_pred, "id":ID}
+	ID = ID+1
 	return block
 
 #funzione che popola ricorsivamente la lista dei blocchi fino a profondità n
