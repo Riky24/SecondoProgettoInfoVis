79c475f6c2d89fa052d917fb61506ce749649ee4
Riccardo <riky24.mo@gmail.com>
2021-07-24 16:27:49 +0200

    Aggiornamento creazione Json

--- dataSet.py
+++ dataSet.py
@@ -33,29 +33,51 @@ def creaFigli(block_pred, lista, n):
 		p_cut = random.uniform(0.0, 1.0)  #valore relativo alla probabilità di taglio della biforcazione
 		if bif>0 and p_cut>=P2:
 			bif = bif-1
+			return [lista, None]
 		else:
 			block = creaBlocco(block_pred, height_pred)
 			lista.append(block)
-			creaFigli(block, lista, n)
+			[lista, b1] = creaFigli(block, lista, n)
 			p_bif = random.uniform(0.0, 1.0)  #valore relativo alla probabilità di creazione della biforcazione
+			b2 = None
 			if p_bif>=P1:
 				block2 = creaBlocco(block_pred, height_pred)
 				bif = bif+1
 				lista.append(block2)
-				creaFigli(block2, lista, n)
+				[lista, b2] = creaFigli(block2, lista, n)
+			if(b2):
+				return [lista, b2]
+			else:
+				return [lista, b1]
+	else: 
+		return [lista, block_pred]
+	
 
 #funzione che genera il file json della blockchain di profondità massima n
 def creaJson(n):
 
 	lista = []
+	listaReturn = []
 	radice = creaBlocco({}, -1)
-	lista.append(radice)
-	creaFigli(radice, lista, n)
-	newList = sorted(lista, key=lambda k: k['height'])
+	b = radice
+	result = [[b], b]
+	while n>0:
+		print("entrato")
+		if n>=2000:
+			result = creaFigli(result[1], result[0], 2000)
+			print(len(result[0]), n)
+			n = n-2000
+		else:
+			result = creaFigli(result[1], result[0], n)
+			n = 0
+		lista = lista.extend(result[0])
+		print(len(lista))
+	newList = sorted(result[0], key=lambda k: k['height'])
+	#print(newList)
 
 	data_set = {"blocks": newList}
 	with open("blockchain.json", "w") as blockchain:
 		json.dump(data_set, blockchain)
 	
 
-creaJson(1100)
+creaJson(20000)
